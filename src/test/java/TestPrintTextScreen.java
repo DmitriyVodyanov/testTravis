ce805d472c7dd5f21116a4b9a7728188fcb5785d
dvodyanov <dmitriy.vodyanov@tortugasocial.com>
2021-09-06 09:54:06 +0300

    add test

--- src/test/java/TestPrintTextScreen.java
+++ src/test/java/TestPrintTextScreen.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 public class TestPrintTextScreen {
 
     private String text = "Hello, World!";
+    private String text1 = "test test test";
+    private String text2 = "new test";
 
     PrintTextScreen printText;
     @Before
@@ -14,7 +16,9 @@ public class TestPrintTextScreen {
 
     @Test
     public void printText() {
-        Assert.assertEquals("Test 1 Hello world", "Hello, World1!", printText.printText(text));
+        Assert.assertEquals("Test 1. Hello world", "Hello, World1!", printText.printText(text));
+        Assert.assertEquals("Test 2. test", "test test test", printText.printText(text1));
+        Assert.assertEquals("Test 3. new test", "new test", printText.printText(text2));
     }
 
 }
